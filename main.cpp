ea72f17b3d00dfa1cf11632ac4a76dee769a56aa
Joshua Patterson <jleegippies@gmail.com>
2021-03-20 13:16:49 -0500

    Added all the points and fixed the distance formula

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <ctgmath>
 
 class Node {
 private:
@@ -39,7 +40,7 @@ Node* Node::getClosest() {
 }
 
 double Node::getDistance(const Node* n) const {
-    return (n->y - y) / (n->x - x);
+    return std::sqrt(std::pow((n->y - y), 2) + std::pow((n->x - x), 2));
 }
 
 bool Node::getIsEnd() const {
@@ -51,18 +52,43 @@ void Node::printCoords() const {
 }
 
 int main() {
-    auto* start = new Node(0, 0, false);
-    auto* oneX = new Node(1, 0, true);
-    auto* oneY = new Node(0, 1, false);
+    auto* start = new Node(1, 2, false);
     auto* oneXOneY = new Node(1, 1, false);
-    auto* dest = new Node(3, 2, false);
+    auto* oneX = new Node(1, 0, false);
+    auto* twoX = new Node(2, 0, false);
+    auto* threeX = new Node(3, 0, false);
 
-    start->addNeighbor(oneX);
-    start->addNeighbor(oneY);
+    auto* threeXOneY = new Node(3, 1, false);
+    auto* threeXTwoY = new Node(3, 2, false);
+    auto* threeXThreeY = new Node(3, 3, false);
+    auto* threeXFourY = new Node(3, 4, false);
+    auto* twoXFourY = new Node(2, 4, false);
+
+    auto* fourX = new Node(4, 0, false);
+    auto* fiveX = new Node(5, 0, false);
+
+    auto* fiveXOneY = new Node(5, 1, false);
+    auto* fiveXTwoY = new Node(5, 2, false);
+    auto* fiveXThreeY = new Node(5, 3, false);
+    auto* fiveXFourY = new Node(5, 4, true);
+
     start->addNeighbor(oneXOneY);
-    oneY->addNeighbor(dest);
-    oneXOneY->addNeighbor(dest);
-    oneX->addNeighbor(dest);
+    oneXOneY->addNeighbor(oneX);
+    oneX->addNeighbor(twoX);
+    twoX->addNeighbor(threeX);
+
+    threeX->addNeighbor(threeXOneY);
+    threeXOneY->addNeighbor(threeXTwoY);
+    threeXTwoY->addNeighbor(threeXThreeY);
+    threeXThreeY->addNeighbor(threeXFourY);
+    threeXFourY->addNeighbor(twoXFourY);
+
+    threeX->addNeighbor(fourX);
+    fourX->addNeighbor(fiveX);
+    fiveX->addNeighbor(fiveXOneY);
+    fiveXOneY->addNeighbor(fiveXTwoY);
+    fiveXTwoY->addNeighbor(fiveXThreeY);
+    fiveXThreeY->addNeighbor(fiveXFourY);
 
     auto current = start->getClosest();
     while (!current->getIsEnd()) {
@@ -74,9 +100,20 @@ int main() {
     std::cout << current->getIsEnd() << std::endl;
 
     delete start;
-    delete oneX;
-    delete oneY;
     delete oneXOneY;
-    delete dest;
+    delete oneX;
+    delete twoX;
+    delete threeX;
+    delete threeXOneY;
+    delete threeXTwoY;
+    delete threeXThreeY;
+    delete threeXFourY;
+    delete twoXFourY;
+    delete fourX;
+    delete fiveX;
+    delete fiveXOneY;
+    delete fiveXTwoY;
+    delete fiveXThreeY;
+    delete fiveXFourY;
     return 0;
 }
